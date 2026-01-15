bdb25e836d05c26508833b47c5d789bf47e7a16b
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-15 12:20:43 +0200

    feat: Add Calibri font and update pubspec.yaml for font asset inclusion

---
+++ lib/core/constants.dart
@@ -0,0 +1,6 @@
+const int khorizontalPadding = 16;
+const int kbottomPadding = 69;
+const int ktopPadding = 16;
+const int kbuttonRadius = 10;
+const int ktextFieldRadius = 8;
+const String kfontFamily = "Calibri";
