283148ea64ff749aefcb50ef7f0aeaa193a21e67
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-15 11:04:41 +0200

    feat: Implement service locator setup in main application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:baby_care/baby_care_app.dart';
+import 'package:baby_care/core/services/get_it_service.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  setupServiceLocator();
   runApp(const BabyCareApp());
 }
