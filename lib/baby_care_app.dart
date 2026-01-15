d38d9441a7a2c236d075437379ce1b2e5a7ed536
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-15 13:43:32 +0200

    feat: Add localization support with Arabic and English messages

--- lib/baby_care_app.dart
+++ lib/baby_care_app.dart
@@ -1,6 +1,8 @@
 import 'package:baby_care/core/helper_functions/on_generate_routes.dart';
 import 'package:baby_care/core/theme/app_theme.dart';
+import 'package:baby_care/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class BabyCareApp extends StatelessWidget {
   const BabyCareApp({super.key});
@@ -8,6 +10,14 @@ class BabyCareApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale('ar'),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       theme: AppTheme.lightTheme,
