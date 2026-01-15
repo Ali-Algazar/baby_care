6052fb1d079aaaef1c2ed94ea1df254546c2de9c
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-15 11:33:13 +0200

    feat: Implement routing functionality with onGenerateRoute method

--- lib/baby_care_app.dart
+++ lib/baby_care_app.dart
@@ -1,3 +1,4 @@
+import 'package:baby_care/core/helper_functions/on_generate_routes.dart';
 import 'package:flutter/material.dart';
 
 class BabyCareApp extends StatelessWidget {
@@ -5,6 +6,9 @@ class BabyCareApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false);
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: onGenerateRoute,
+    );
   }
 }
