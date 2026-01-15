f020a909016b9013b846b9f0452f94fb313a79d9
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-15 11:25:20 +0200

    feat: Implement ApiHelper for API interactions with authentication support

---
+++ lib/core/services/api_helper.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import '../helper/shared_preferences_service.dart';
+
+class ApiHelper {
+  Dio dio;
+  final String baseUrl = 'https://mom-hf4l.vercel.app/api/v1/';
+
+  ApiHelper(this.dio);
+
+  Future<String?> _getAuthToken() async {
+    return await SharedPreferencesService.getData(key: 'auth_token');
+  }
+
+  Future<Options?> _createAuthOptions(bool requiresAuth) async {
+    if (requiresAuth) {
+      String? token = await _getAuthToken();
+      if (token != null) {
+        return Options(headers: {'Authorization': 'Bearer $token'});
+      }
+    }
+    return null;
+  }
+
+  Future<Response> get(
+    String endpoint, {
+    Map<String, dynamic>? data,
+    bool requiresAuth = false,
+  }) async {
+    Options? options = await _createAuthOptions(requiresAuth);
+
+    return await dio.get(
+      '$baseUrl$endpoint',
+      queryParameters: data,
+      options: options,
+    );
+  }
+
+  Future<Response> post(
+    String endpoint, {
+    Map<String, dynamic>? data,
+    bool requiresAuth = false,
+  }) async {
+    Options? options = await _createAuthOptions(requiresAuth);
+    return await dio.post('$baseUrl$endpoint', data: data, options: options);
+  }
+
+  Future<Response> put(
+    String endpoint, {
+    Map<String, dynamic>? data,
+    bool requiresAuth = false,
+  }) async {
+    Options? options = await _createAuthOptions(requiresAuth);
+    return await dio.put('$baseUrl$endpoint', data: data, options: options);
+  }
+
+  Future<Response> delete(
+    String endpoint, {
+    Map<String, dynamic>? data,
+    bool requiresAuth = false,
+  }) async {
+    Options? options = await _createAuthOptions(requiresAuth);
+    return await dio.delete('$baseUrl$endpoint', data: data, options: options);
+  }
+}
