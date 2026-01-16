bce7a1ec81e85ace86073b9c63325185de5ff891
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-16 14:50:06 +0200

    feat: Create OnboardingPage widget for onboarding flow

---
+++ lib/features/onboarding/presentation/view/widgets/onboarding_page.dart
@@ -0,0 +1,76 @@
+import 'package:baby_care/core/constants.dart';
+import 'package:baby_care/core/extensions/extensions.dart';
+import 'package:baby_care/core/utils/app_colors.dart';
+import 'package:baby_care/core/utils/app_text_styles.dart';
+import 'package:baby_care/core/widgets/custom_button.dart';
+import 'package:baby_care/features/onboarding/presentation/model/onboarding_model.dart';
+import 'package:baby_care/features/onboarding/presentation/view/widgets/custom_onboarding_app_bar.dart';
+import 'package:baby_care/generated/l10n.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({
+    super.key,
+    required this.onboardings,
+    required this.index,
+  });
+
+  final OnboardingModel onboardings;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ktopPadding.height,
+          Stack(
+            children: [
+              Image.asset(
+                onboardings.image,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+              CustomOnBoardingAppBar(index: index),
+            ],
+          ),
+
+          50.height,
+
+          Padding(
+            padding: khorizontalPadding.horizontal,
+            child: Column(
+              children: [
+                Text(
+                  onboardings.title,
+                  style: AppTextStyles.headerBold.copyWith(
+                    fontSize: 24,
+                    fontFamily: kfontFamily,
+                  ),
+                ),
+                16.height,
+                Text(
+                  onboardings.description,
+                  style: AppTextStyles.body1Ragular.copyWith(
+                    fontSize: 20,
+                    color: AppColors.dText,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                70.height,
+
+                CustomButton(
+                  title: index == 2
+                      ? S.of(context).startNow
+                      : S.of(context).next,
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          kbottomPadding.height,
+        ],
+      ),
+    );
+  }
+}
--- lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:baby_care/core/constants.dart';
-import 'package:baby_care/core/utils/app_colors.dart';
-import 'package:baby_care/core/utils/app_text_styles.dart';
-import 'package:baby_care/core/extensions/extensions.dart';
 import 'package:baby_care/features/onboarding/presentation/data/onboarding_items_list.dart';
 import 'package:baby_care/features/onboarding/presentation/model/onboarding_model.dart';
-import 'package:baby_care/generated/l10n.dart';
+import 'package:baby_care/features/onboarding/presentation/view/widgets/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingViewBody extends StatefulWidget {
@@ -24,58 +20,11 @@ class _OnboardingViewBodyState extends State<OnboardingViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ktopPadding.height,
-        Stack(
-          children: [
-            Image.asset(
-              onboardings[0].image,
-              fit: BoxFit.cover,
-              width: double.infinity,
-            ),
-            Padding(
-              padding: khorizontalPadding.horizontal,
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_back_outlined),
-                  ),
-                  Spacer(),
-                  Text(S.of(context).skip, style: AppTextStyles.body2Bold),
-                ],
-              ),
-            ),
-          ],
-        ),
-
-        Text(
-          onboardings[0].title,
-          style: AppTextStyles.headerBold.copyWith(
-            fontSize: 24,
-            fontFamily: kfontFamily,
-          ),
-        ),
-        Text(
-          onboardings[0].description,
-          style: AppTextStyles.body1Ragular.copyWith(
-            fontSize: 20,
-            color: AppColors.dText,
-          ),
-          textAlign: TextAlign.center,
-        ),
-
-        Container(
-          width: double.infinity,
-          height: 46,
-          decoration: BoxDecoration(
-            gradient: AppColors.primaryL,
-            borderRadius: BorderRadius.circular(kbuttonRadius.toDouble()),
-          ),
-          child: Center(child: Text('jhgjh', style: AppTextStyles.btnsBold)),
-        ),
-      ],
+    return PageView.builder(
+      itemCount: onboardings.length,
+      itemBuilder: (context, index) {
+        return OnboardingPage(onboardings: onboardings[index], index: index);
+      },
     );
   }
 }
