f1123aa25aac28f42115c716f0621c3033918e33
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-19 14:53:06 +0200

    feat: Implement AuthRepository interface for authentication operations

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:baby_care/core/errors/failures.dart';
+import 'package:baby_care/features/auth/data/model/user_model.dart';
+
+import 'package:dartz/dartz.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, UserModel>> login(String email, String password);
+  Future<Either<Failure, UserModel>> register(
+    String email,
+    String password,
+    String nationalId, {
+    String? displayName,
+  });
+  Future<Either<Failure, void>> logout();
+}
