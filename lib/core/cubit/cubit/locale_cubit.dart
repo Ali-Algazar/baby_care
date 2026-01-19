36fea06195bea32a78f872c100902f2b4961dc30
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-19 11:12:56 +0200

    feat: Refactor changeLocale method in LocaleCubit to accept locale parameter

--- lib/core/cubit/cubit/locale_cubit.dart
+++ lib/core/cubit/cubit/locale_cubit.dart
@@ -3,11 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class LocaleCubit extends Cubit<String> {
   LocaleCubit() : super('ar');
 
-  void changeLocale() {
-    if (state == 'ar') {
-      emit('en');
-    } else {
-      emit('ar');
-    }
+  void changeLocale(String locale) {
+    emit(locale);
   }
 }
