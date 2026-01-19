e6579e5ea2384d745f71fe8a068df4abf25e13d7
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-19 18:21:26 +0200

    feat: Implement AuthLocalDataSource for user caching and token management

---
+++ lib/features/auth/data/datasources/auth_local_data_source.dart
@@ -0,0 +1,64 @@
+import 'package:baby_care/core/constants.dart';
+import 'package:baby_care/core/helper/hive_helper.dart';
+import 'package:baby_care/core/helper/shared_preferences_service.dart';
+import 'package:baby_care/features/auth/data/model/user_model.dart';
+import 'package:hive/hive.dart';
+
+abstract class AuthLocalDataSource {
+  Future<void> cacheUser(UserModel user);
+  Future<UserModel?> getCachedUser();
+  Future<void> clearCachedUser();
+  Future<void> cacheUserToken(String token);
+  Future<String> getCachedUserToken();
+  Future<void> clearCachedUserToken();
+}
+
+class AuthLocalDataSourceImpl implements AuthLocalDataSource {
+  @override
+  Future<void> cacheUser(UserModel user) async {
+    await HiveHelper.putData(
+      boxName: Constants.userBox,
+      key: Constants.userHiveKey,
+      value: user,
+    );
+  }
+
+  @override
+  Future<void> cacheUserToken(String token) async {
+    await SharedPreferencesService.saveData(
+      key: Constants.tokeneKey,
+      value: token,
+    );
+  }
+
+  @override
+  Future<void> clearCachedUser() async {
+    await HiveHelper.deleteData(
+      boxName: Constants.userBox,
+      key: Constants.userHiveKey,
+    );
+  }
+
+  @override
+  Future<void> clearCachedUserToken() async {
+    await SharedPreferencesService.removeData(key: Constants.tokeneKey);
+  }
+
+  @override
+  Future<UserModel?> getCachedUser() async {
+    final user = await HiveHelper.getData(
+      boxName: Constants.userBox,
+      key: Constants.userHiveKey,
+    );
+    return user;
+  }
+
+  @override
+  Future<String> getCachedUserToken() async {
+    var token = await SharedPreferencesService.getData(
+      key: Constants.tokeneKey,
+    );
+
+    return token;
+  }
+}
