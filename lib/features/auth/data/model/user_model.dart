2454ff6aa2d6b4378f76bba968a340647f804206
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-19 14:22:39 +0200

    feat: Add UserModel class and update dependencies in pubspec.yaml and pubspec.lock

---
+++ lib/features/auth/data/model/user_model.dart
@@ -0,0 +1,19 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+
+  const UserModel({required this.id, required this.name, required this.email});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['_id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'_id': id, 'name': name, 'email': email};
+  }
+}
