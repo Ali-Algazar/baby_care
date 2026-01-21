024cc324487db02d6e25938076dfb6510eb1143c
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-21 10:15:25 +0200

    feat: add SVG assets for Baby, notifications, and switch icons; update CircleAvatarLinearColor to support linear gradient

--- lib/core/widgets/circle_avatar_linear_color.dart
+++ lib/core/widgets/circle_avatar_linear_color.dart
@@ -1,22 +1,28 @@
+import 'package:baby_care/core/utils/app_colors.dart';
 import 'package:baby_care/core/widgets/widget_linear_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class CircleAvatarLinearColor extends StatelessWidget {
   const CircleAvatarLinearColor({
     super.key,
     required this.radius,
     required this.child,
+    this.linearGradient = AppColors.primaryL,
   });
   final double radius;
   final Widget child;
-
+  final LinearGradient linearGradient;
   @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: AlignmentGeometry.center,
       children: [
-        WidgetLinearColor(widget: CircleAvatar(radius: radius)),
+        FittedBox(
+          child: WidgetLinearColor(
+            widget: CircleAvatar(radius: radius),
+            linearGradient: linearGradient,
+          ),
+        ),
 
         child,
       ],
