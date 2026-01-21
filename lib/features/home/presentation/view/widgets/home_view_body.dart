9f138004b4a2c1520ed017001eac511c35fc2738
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-21 10:16:07 +0200

    feat: implement CustomHomeAppBar and integrate user data fetching in HomeViewBody

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,10 +1,48 @@
+import 'package:baby_care/core/constants.dart';
+import 'package:baby_care/core/extensions/extensions.dart';
+import 'package:baby_care/core/helper/hive_helper.dart';
+import 'package:baby_care/features/auth/data/model/user_model.dart';
+import 'package:baby_care/features/home/presentation/view/widgets/custom_home_app_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
 
   @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  Future<UserModel> fitchedUser() async {
+    var user = await HiveHelper.getData(
+      boxName: Constants.userBox,
+      key: Constants.userHiveKey,
+    );
+    return user;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home View Body'));
+    return FutureBuilder(
+      future: fitchedUser(),
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (asyncSnapshot.hasError) {
+          return Center(child: Text('Error: ${asyncSnapshot.error}'));
+        }
+        if (asyncSnapshot.hasData) {
+          UserModel user = asyncSnapshot.data!;
+          return Column(
+            children: [
+              Constants.ktopPadding.height,
+              CustomHomeAppBar(user: user),
+            ],
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
   }
 }
