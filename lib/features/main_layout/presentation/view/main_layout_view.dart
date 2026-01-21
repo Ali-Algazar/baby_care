3b23af1d2f605e6b99010d1d1d6fb2817ef259ab
Ali Mohamed <ilamohemed12@gmail.com>
2026-01-21 07:28:17 +0200

    feat: Add Main Layout View and update navigation in SignInViewBody, SignUpViewBody, and SplashViewBody

---
+++ lib/features/main_layout/presentation/view/main_layout_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainLayoutView extends StatefulWidget {
+  const MainLayoutView({super.key});
+  static const routeName = '/main-layout';
+
+  @override
+  State<MainLayoutView> createState() => _MainLayoutViewState();
+}
+
+class _MainLayoutViewState extends State<MainLayoutView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
