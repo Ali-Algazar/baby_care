386d4483b89b53e5114c5742a4e6b869fd71ffad
Ali Mohamed <ilamohamed12@gmail.com>
2026-01-27 08:03:24 +0200

    feat: refactor ChildrenCubit and ChildrenState to include repository dependency and loading states

--- lib/features/children/presentation/cubit/children_cubit.dart
+++ lib/features/children/presentation/cubit/children_cubit.dart
@@ -1,6 +1,19 @@
+import 'package:baby_care/features/children/data/repositories/children_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'children_state.dart';
 
 class ChildrenCubit extends Cubit<ChildrenState> {
-  ChildrenCubit() : super(ChildrenInitial());
+  ChildrenCubit({required this.repository}) : super(ChildrenInitial());
+  ChildrenRepository repository;
+
+  Future<void> getChildren() async {
+    emit(ChildrenLoading());
+    var result = await repository.getChildren();
+    result.fold((l) => emit(ChildrenError(message: l.message)), (r) {
+      if (r.isEmpty) {
+        emit(ChildrenEmpty());
+      }
+      emit(ChildrenLoaded(children: r));
+    });
+  }
 }
--- lib/features/children/presentation/cubit/children_state.dart
+++ lib/features/children/presentation/cubit/children_state.dart
@@ -1,3 +1,21 @@
+import 'package:baby_care/features/children/data/model/child_model.dart';
+
 abstract class ChildrenState {}
 
 class ChildrenInitial extends ChildrenState {}
+
+class ChildrenLoading extends ChildrenState {}
+
+class ChildrenLoaded extends ChildrenState {
+  final List<ChildModel> children;
+
+  ChildrenLoaded({required this.children});
+}
+
+class ChildrenError extends ChildrenState {
+  final String message;
+
+  ChildrenError({required this.message});
+}
+
+class ChildrenEmpty extends ChildrenState {}
