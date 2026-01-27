2f41d07e50c34ec6e3192e210fa914811e68d426
Ali Mohamed <ilamohamed12@gmail.com>
2026-01-27 08:03:08 +0200

    feat: update getChildren method in ChildrenRepositoryImpl to handle connectivity and caching

--- lib/features/children/data/repositories/children_repository.dart
+++ lib/features/children/data/repositories/children_repository.dart
@@ -1,4 +1,7 @@
+import 'package:baby_care/core/errors/failures.dart';
+import 'package:baby_care/features/children/data/model/child_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class ChildrenRepository {
-  // Future<void> getChildren(String id);
+  Future<Either<Failure, List<ChildModel>>> getChildren();
 }
--- lib/features/children/data/repositories/children_repository_impl.dart
+++ lib/features/children/data/repositories/children_repository_impl.dart
@@ -1,20 +1,52 @@
+import 'package:baby_care/core/errors/failures.dart';
+import 'package:baby_care/features/children/data/datasources/children_local_data_source.dart';
+import 'package:baby_care/features/children/data/datasources/children_remote_data_source.dart';
+import 'package:baby_care/features/children/data/model/child_model.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dartz/dartz.dart';
 
 import 'children_repository.dart';
-// (اختياري) يمكنك استيراد الـ datasources
-// import '../datasources/children_remote_data_source.dart';
-// import '../datasources/children_local_data_source.dart';
 
 class ChildrenRepositoryImpl implements ChildrenRepository {
-  // final ChildrenRemoteDataSource remoteDataSource;
-  // final ChildrenLocalDataSource localDataSource;
+  final ChildrenRemoteDataSource remoteDataSource;
+  final ChildrenLocalDataSource localDataSource;
+  final Connectivity connectivity;
 
-  // ChildrenRepositoryImpl({
-  //   required this.remoteDataSource,
-  //   required this.localDataSource,
-  // });
+  ChildrenRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.connectivity,
+  });
+  @override
+  Future<Either<Failure, List<ChildModel>>> getChildren() async {
+    try {
+      final hasConnection1 = await hasConnection();
+      if (hasConnection1) {
+        var response = await remoteDataSource.getChildren();
+        if (response.statusCode == 200) {
+          List<ChildModel> children = (response.data as List).map((child) {
+            return ChildModel.fromJson(child);
+          }).toList();
+          await localDataSource.cacheChildrenList(children);
 
-  // @override
-  // Future<void> getChildren(String id) async {
-  //   // ...
-  // }
+          return right(await localDataSource.getCachedChildrenList());
+        } else {
+          return left(ServerFailure('error '));
+        }
+      } else {
+        var listChildren = await localDataSource.getCachedChildrenList();
+        return right(listChildren);
+      }
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  Future<bool> hasConnection() async {
+    final results = await connectivity.checkConnectivity();
+    final hasConnection =
+        results.isNotEmpty && results.first != ConnectivityResult.none;
+
+    return hasConnection;
+  }
 }
