304ee266e5f9de163e815ad172f0e430c69f5c87
Ali Mohamed <ilamohamed12@gmail.com>
2026-02-03 13:17:02 +0200

    feat: add ArticleModel and ArticleModelAdapter for blog data management

---
+++ lib/features/blog/data/model/article_model.dart
@@ -0,0 +1,46 @@
+import 'package:hive/hive.dart';
+part 'article_model.g.dart';
+
+@HiveType(typeId: 7)
+class ArticleModel {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String title;
+  @HiveField(2)
+  final String content;
+  @HiveField(3)
+  final String category;
+  @HiveField(4)
+  final String imageUrl;
+  @HiveField(5)
+  final String author;
+  @HiveField(6)
+  final DateTime createdAt;
+  @HiveField(7)
+  final DateTime updatedAt;
+
+  ArticleModel({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.category,
+    required this.imageUrl,
+    required this.author,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      id: json['_id'],
+      title: json['title'],
+      content: json['content'],
+      category: json['category'],
+      imageUrl: json['imageUrl'],
+      author: json['author'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+    );
+  }
+}
