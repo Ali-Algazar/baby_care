f8215b0802595fb607ab7674b43ae0dfcfc3ff54
Ali Mohamed <ilamohamed12@gmail.com>
2026-02-03 13:34:08 +0200

    feat: implement BlogRemoteDataSource for fetching blog articles from the API

---
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -0,0 +1,19 @@
+import 'package:baby_care/core/services/api_helper.dart';
+import 'package:baby_care/core/utils/end_points.dart';
+import 'package:dio/dio.dart';
+
+abstract class BlogRemoteDataSource {
+  Future<Response> fetchBlogArticles();
+}
+
+class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
+  final ApiHelper apiHelper;
+
+  BlogRemoteDataSourceImpl({required this.apiHelper});
+
+  @override
+  Future<Response> fetchBlogArticles() async {
+    final response = await apiHelper.get(ApiEndpoints.articles);
+    return response;
+  }
+}
