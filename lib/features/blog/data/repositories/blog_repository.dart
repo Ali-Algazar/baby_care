f5dcab53334253bf51f384dcdac99e769fcb8968
Ali Mohamed <ilamohamed12@gmail.com>
2026-02-03 13:44:29 +0200

    feat: implement BlogRepository and BlogRepositoryImpl for fetching and caching blog articles

---
+++ lib/features/blog/data/repositories/blog_repository.dart
@@ -0,0 +1,7 @@
+import 'package:baby_care/core/errors/failures.dart';
+import 'package:baby_care/features/blog/data/model/article_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class BlogRepository {
+  Future<Either<Failure, List<ArticleModel>>> fetchBlogArticles();
+}
---
+++ lib/features/blog/data/repositories/blog_repository_impl.dart
@@ -0,0 +1,55 @@
+import 'package:baby_care/core/errors/failures.dart';
+import 'package:baby_care/features/blog/data/datasources/blog_local_data_source.dart';
+import 'package:baby_care/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:baby_care/features/blog/data/model/article_model.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import 'blog_repository.dart';
+
+class BlogRepositoryImpl implements BlogRepository {
+  final BlogRemoteDataSource remoteDataSource;
+  final BlogLocalDataSource localDataSource;
+  final Connectivity connectivity;
+  BlogRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.connectivity,
+  });
+
+  @override
+  Future<Either<Failure, List<ArticleModel>>> fetchBlogArticles() async {
+    try {
+      if (await hasConnection()) {
+        final respons = await remoteDataSource.fetchBlogArticles();
+        if (respons.statusCode == 200) {
+          final remoteArticles = (respons.data['data'] as List)
+              .map((e) => ArticleModel.fromJson(e))
+              .toList();
+          localDataSource.cacheBlogArticles(remoteArticles);
+          return right(remoteArticles);
+        } else {
+          return left(ServerFailure('there was ${respons.statusCode} error'));
+        }
+        // localDataSource.cacheBlogArticles(remoteArticles);
+        // return right(remoteArticles);
+      } else {
+        final localArticles = await localDataSource.getCachedBlogArticles();
+        return right(localArticles as List<ArticleModel>);
+      }
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  Future<bool> hasConnection() async {
+    final results = await connectivity.checkConnectivity();
+    final hasConnection =
+        results.isNotEmpty && results.first != ConnectivityResult.none;
+
+    return hasConnection;
+  }
+}
