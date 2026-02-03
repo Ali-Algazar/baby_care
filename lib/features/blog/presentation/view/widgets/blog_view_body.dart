44774df8c007b5ad23827456bfe9f75cd6f6acec
Ali Mohamed <ilamohamed12@gmail.com>
2026-02-03 14:11:28 +0200

    feat: implement BlocBuilder for blog article state management in BlogViewBody

--- lib/features/blog/presentation/view/widgets/blog_view_body.dart
+++ lib/features/blog/presentation/view/widgets/blog_view_body.dart
@@ -1,8 +1,11 @@
 import 'package:baby_care/core/utils/app_colors.dart';
 import 'package:baby_care/core/utils/app_text_styles.dart';
+import 'package:baby_care/features/blog/presentation/cubit/blog_cubit.dart';
+import 'package:baby_care/features/blog/presentation/cubit/blog_state.dart';
 import 'package:baby_care/features/home/presentation/view/widgets/articles_care_widget.dart';
 import 'package:baby_care/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class BlogViewBody extends StatelessWidget {
@@ -10,41 +13,70 @@ class BlogViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: Row(
-              children: [
-                Text(
-                  S.of(context).allArticles,
-                  style: AppTextStyles.headerBold,
-                ),
-                const Spacer(),
-                Container(
-                  padding: const EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    border: Border.all(color: AppColors.border),
+    return BlocBuilder<BlogCubit, BlogState>(
+      builder: (context, state) {
+        if (state is BlogLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is BlogError) {
+          return Center(child: Text(state.message));
+        }
+        if (state is BlogLoaded) {
+          return CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
                   ),
-                  child: SvgPicture.asset('assets/svg/leading.svg'),
+                  child: Row(
+                    children: [
+                      Text(
+                        S.of(context).allArticles,
+                        style: AppTextStyles.headerBold,
+                      ),
+                      const Spacer(),
+                      Container(
+                        padding: const EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          border: Border.all(color: AppColors.border),
+                        ),
+                        child: SvgPicture.asset('assets/svg/leading.svg'),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
+
+              SliverList.builder(
+                itemCount: 15,
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 16.0,
+                    left: 16.0,
+                    right: 16.0,
+                  ),
+                  child: ArticlesCareWidget(
+                    articleModel: state.articles[index],
+                  ),
+                ),
+              ),
+
+              const SliverToBoxAdapter(child: SizedBox(height: 80)),
+            ],
+          );
+        }
+        if (state is BlogEmpty) {
+          return Center(
+            child: Text(
+              ' S.of(context).noArticlesAvailable',
+              style: AppTextStyles.headerBold,
             ),
-          ),
-        ),
-
-        SliverList.builder(
-          itemCount: 15,
-          itemBuilder: (context, index) => const Padding(
-            padding: EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
-            child: ArticlesCareWidget(),
-          ),
-        ),
-
-        const SliverToBoxAdapter(child: SizedBox(height: 80)),
-      ],
+          );
+        }
+        return const SizedBox();
+      },
     );
   }
 }
